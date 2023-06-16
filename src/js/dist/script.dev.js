e73f3dbacf9165f635b063737c7b679f422d1180
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-06-16 12:46:44 +0200

    select.menuProduct.clickable - confusion

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -111,7 +111,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
         var thisProduct = this;
         /* find the clickable trigger (the element that should react to clicking) */
 
-        var clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); //thisProduct.accordionTrigger();
+        var clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); //thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
         /* START: add event listener to clickable trigger on event click */
 
